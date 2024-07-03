c3395f7568c0e208ead677262ee8328533bc17be
Santiago Andermatten <santiagoandermatten1@gmail.com>
2024-07-03 15:29:10 -0300

    fin

--- config/database.py
+++ config/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 
 from sqlalchemy.orm import sessionmaker, declarative_base
-DATABASE_URL = "mysql+mysqlconnector://root:admin@localhost:3306/tpi"
+DATABASE_URL = "mysql+mysqlconnector://root:IKfGnwoLmHnNxoJNfABdWQMcTXJRCFpS@roundhouse.proxy.rlwy.net:33173/railway"
 
 engine = create_engine(DATABASE_URL)
 
