bc2606eb3451b942145c1ea8a25e00fd5bb7c7a6
Santiago Andermatten <santiagoandermatten1@gmail.com>
2024-07-03 12:03:28 -0300

    cambio textos jwt

--- utilss/jwt_manager.py
+++ utilss/jwt_manager.py
@@ -9,10 +9,14 @@ def create_token(data: dict):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=30)
     to_encode.update({"exp": expire})
-    new_token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    new_token = encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return new_token
 
-
-def validate_token(token: str) :
-    data: dict = decode(token, key="my_secret_key", algorithms=['HS256'])
-    return data
+def validate_token(token: str) -> dict:
+    try:
+        data = decode(token, key=SECRET_KEY, algorithms=[ALGORITHM])
+        return data
+    except jwt.ExpiredSignatureError:
+        raise Exception("Token expirado")
+    except jwt.InvalidTokenError:
+        raise Exception("Token invalido")
